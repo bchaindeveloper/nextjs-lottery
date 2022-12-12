b42fa182f392daf96d2a5183b6373623ba8c73a6
bchaindeveloper <bcdev3@protonmail.com>
2022-12-12 15:30:40 -0500

    Href

--- components/Footer.js
+++ components/Footer.js
@@ -2,13 +2,13 @@
 export default function Footer() {
     return (
     
-    <div className="fixed bottom-0 p-5 border-b-3 flex "
+    <div className="fixed bottom-0 p-5 border-b-3 flex"
     style={{
         backgroundColor: 'black',
         width: '100%',
         height: '100px'
       }}>
-        <a href="https://bchaindev.com/" alt="BChainDev" target="_blank">
+        <a href="https://github.com/bchaindeveloper/smartcontract-lottery" alt="BChainDev" target="_blank" rel="noreferrer">
             <h1 className="py-4 px-4 font-bold text-3xl text-white">BChainDev Production</h1>
         </a>
             <p className="pr-4 bottom-2 font-normal text-[14px] text-white fixed right-0">
